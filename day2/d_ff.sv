fd11bd7fd354f8d872893c49b856916a52340b8c
kevinpinto98 <kevinpinto98@gmail.com>
2022-07-02 00:49:18 +0530

    new commit

--- day2/d_ff.sv
+++ day2/d_ff.sv
@@ -1,4 +1,26 @@
-module d_ff();
+module d_ff(q,q_async,q_sync,d,clk,reset);
+    input d,clk,reset;
+    output reg q, q_async, q_sync;
 
+    always @(posedge clk)
+    begin
+        q <= d;
+    end
+    
+    always @(posedge clk or negedge reset)
+    begin
+        if(!reset)
+        q <= 1'b0;
+        else
+        q_async <= d;
+    end
+
+    always @(posedge clk)
+    begin
+        if(!reset)
+        q <= 1'b0;
+        else
+        q_sync <= d;
+    end
 
 endmodule
