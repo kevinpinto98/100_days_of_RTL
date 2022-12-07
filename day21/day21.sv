d07cf2833ddeadee7c81d427e8f02ba9e71bb9e7
Kevin Pinto <kevinpinto98@gmail.com>
2022-12-07 20:12:52 +0100

    day21

---
+++ day21/day21.sv
@@ -0,0 +1,11 @@
+class day21;
+
+function new();
+//Nothing to do here
+endfunction
+
+function void print();
+$display("Hi! This is day21 of the #100_day_RTL challenge");
+endfunction
+
+endclass
---
+++ day21/day21_tb.sv
@@ -0,0 +1,12 @@
+module day21_tb();
+
+day21 Day21;
+
+initial begin
+    Day21 = new();
+    Day21.print();
+    $finish();
+end
+
+
+endmodule
